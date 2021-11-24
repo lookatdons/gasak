b5e84b39d9793f470bcca181fb51d2565e85ecbc
lookatdons <lookatdons@users.noreply.github.com>
2021-11-24 13:20:36 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,1 +1,2 @@
-<script data-ad-client="ca-pub-2108912098413782" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2108912098413782"
+     crossorigin="anonymous"></script>
