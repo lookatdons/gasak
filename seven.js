8bd6379f2920a4706a514fc3844a230d56246254
lookatdons <lookatdons@users.noreply.github.com>
2021-11-24 13:49:31 +0700

    Delete seven.js

--- seven.js
+++
@@ -1,8 +0,0 @@
-atOptions = {
-		'key' : '159168254d603e18fe7e5278db5f9a50',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://mommygravelyslime.com/159168254d603e18fe7e5278db5f9a50/invoke.js"></scr' + 'ipt>');
