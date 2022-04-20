568c095891a904927ee40095126cd077cfeeb85c
lookatdons <lookatdons@users.noreply.github.com>
2022-04-20 22:26:13 +0700

    Create low.js

---
+++ low.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '61512fb2273c61f1bcacc2c02527bd52',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformats.com/61512fb2273c61f1bcacc2c02527bd52/invoke.js"></scr' + 'ipt>');
