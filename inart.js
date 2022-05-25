734203e388321cba4ceb311260b06dd2a6cd3b34
lookatdons <lookatdons@users.noreply.github.com>
2022-05-25 15:39:59 +0700

    Update inart.js

--- inart.js
+++ inart.js
@@ -1,1 +1,10 @@
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block; text-align:center;"
+     data-ad-layout="in-article"
+     data-ad-format="fluid"
+     data-ad-client="ca-pub-9195111740302021"
+     data-ad-slot="7611937960"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
