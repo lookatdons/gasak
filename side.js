d7fb993b5056c93c45a19d482f9dd6bbb02bdcc0
lookatdons <lookatdons@users.noreply.github.com>
2022-05-25 17:48:06 +0700

    Delete side.js

--- side.js
+++
@@ -1,9 +0,0 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 300x250 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:250px"
-     data-ad-client="ca-pub-9195111740302021"
-     data-ad-slot="1859647990"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
