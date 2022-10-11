8818c095ba91b6d19b81013c7c5cc0756274be2f
lookatdons <lookatdons@users.noreply.github.com>
2022-10-11 14:17:39 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://tokopedia.link/sVmmLYFjQtb"); 
+      location.replace("https://amzn.to/3rI97c2"); 
     },10); 
   } 
 }, false); 
