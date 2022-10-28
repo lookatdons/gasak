d9a6420e07359f212e70a4c6663346d9f560daa9
lookatdons <lookatdons@users.noreply.github.com>
2022-10-28 12:04:24 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://foolproofoptions.com/?via=lookatdons"); 
+      location.replace("https://www.agoda.com/partners/partnersearch.aspx?pcs=1&cid=1911772&hl=en-us"); 
     },10); 
   } 
 }, false); 
