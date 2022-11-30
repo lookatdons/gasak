28e6453cd253418cf7549fc903af9a61cb82c35f
lookatdons <lookatdons@users.noreply.github.com>
2022-11-30 19:47:07 +0700

    Update facedew728.js

--- facedew728.js
+++ facedew728.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : 'bd7e1f47419a47c4b49cb33cef406d2c',
 		'format' : 'iframe',
 		'height' : 90,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://mommygravelyslime.com/bd7e1f47419a47c4b49cb33cef406d2c/invoke.js"></scr' + 'ipt>');
-</script>
