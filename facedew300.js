4212a86237f0521e63e3952190e6bd2bb77ff9db
lookatdons <lookatdons@users.noreply.github.com>
2022-11-30 19:47:28 +0700

    Update facedew300.js

--- facedew300.js
+++ facedew300.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '62fd959ca286368ae7c419eef5d59291',
 		'format' : 'iframe',
 		'height' : 250,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://mommygravelyslime.com/62fd959ca286368ae7c419eef5d59291/invoke.js"></scr' + 'ipt>');
-</script>
