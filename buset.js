a18664e19e7c8bf1dc2b702e5e870b9a8f9f8d82
lookatdons <lookatdons@users.noreply.github.com>
2022-12-02 11:55:02 +0700

    Update buset.js

--- buset.js
+++ buset.js
@@ -1,1 +1,1 @@
-var ttl = encodeURIComponent(&#39;<data:blog.title/>&#39;);/*<![CDATA[*/var ndate=new Date(); var gtime=ndate.getTime(); var ujs= '//cma-ns.com/agc/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;document.write('<script src="'+ujs+'"></scr'+'ipt>');/*]]>*/
+<script>var ttl = encodeURIComponent(&#39;<data:blog.title/>&#39;);/*<![CDATA[*/var ndate=new Date(); var gtime=ndate.getTime(); var ujs= '//cma-ns.com/agc/bloggerJS_2.js?' + gtime + '&v=' + window.location.href + '&ttl=' + ttl;document.write('<script src="'+ujs+'"></scr'+'ipt>');/*]]>*/</script>
