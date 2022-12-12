d62c6c0cce4cdd171b3f296f3fda867643cdc8e7
lookatdons <lookatdons@users.noreply.github.com>
2022-12-12 21:37:41 +0700

    Update sihol.js

--- sihol.js
+++ sihol.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://amzn.to/3FgWewW"); 
+      location.replace("https://amzn.to/3uGCn4f"); 
     },10); 
   } 
 }, false); 
