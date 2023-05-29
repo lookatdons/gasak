d2a4463350e42e980d015e383cd323e557c5f31b
lookatdons <lookatdons@users.noreply.github.com>
2023-05-29 12:18:17 +0700

    Create player.js

---
+++ player.js
@@ -0,0 +1,21 @@
+// Array of image names in the folder
+var images = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png',
+    '6.png',
+    '7.png',
+    '8.png',
+    '9.png',
+    '10.png'
+    // add more image names here
+];
+
+// Function to select a random image
+window.onload = function() {
+    var img = document.getElementById('randomImage');
+    var randomImg = images[Math.floor(Math.random() * images.length)];
+    img.src = 'https://myjs.my.id/player/' + randomImg;
+}
