37fef3b6e57c6245ec8d5deedfcb1beced729704
lookatdons <lookatdons@users.noreply.github.com>
2023-05-30 14:02:16 +0700

    Create head.js

---
+++ head.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var titles = [
+      "Hot Mom.mp4",
+      "Step Mom.mp4",
+      "MILF.mp4",
+      "Friend's Hot Mom.mp4",
+      "Hot Stepmom.mp4"
+    ]; // Add as many titles as you want
+    var randomIndex = Math.floor(Math.random() * titles.length);
+    document.getElementById("randomTitle").innerText = titles[randomIndex];
+}
