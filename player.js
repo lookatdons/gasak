2d8337fdedb48b028e01800f0e9a7089f52ccb09
lookatdons <lookatdons@users.noreply.github.com>
2023-05-30 14:18:36 +0700

    Update player.js

--- player.js
+++ player.js
@@ -14,8 +14,26 @@ var images = [
 ];
 
 // Function to select a random image
-window.onload = function() {
+function getRandomImage() {
     var img = document.getElementById('randomImage');
     var randomImg = images[Math.floor(Math.random() * images.length)];
     img.src = 'https://s3.us-west-1.wasabisys.com/fighter/player/' + randomImg;
 }
+
+var titleNames = [
+    "Hot Mom.mp4",
+    "Step Mom.mp4",
+    "Friend's Hot Mom.mp4",
+    "Hot Stepmom.mp4"
+    // add more titles as needed
+];
+
+function getRandomTitle() {
+    var randomIndex = Math.floor(Math.random() * titleNames.length);
+    document.getElementById('randomTitle').textContent = titleNames[randomIndex];
+}
+
+window.onload = function() {
+    getRandomImage();
+    getRandomTitle();
+};
