cbdc4309baa24940f3177bed06d9ba65ebd3c3ec
lookatdons <lookatdons@users.noreply.github.com>
2023-05-30 13:57:43 +0700

    Update player.js

--- player.js
+++ player.js
@@ -19,9 +19,3 @@ window.onload = function() {
     var randomImg = images[Math.floor(Math.random() * images.length)];
     img.src = 'https://s3.us-west-1.wasabisys.com/fighter/player/' + randomImg;
 }
-
-window.onload = function() {
-    var titles = ["Hot Mom.mp4", "Step Mom.mp4", "MILF.mp4", "Friend's Hot Mom.mp4", "Hot Stepmom.mp4"]; // Add as many titles as you want
-    var randomIndex = Math.floor(Math.random() * titles.length);
-    document.getElementById("randomTitle").innerText = titles[randomIndex];
-}
