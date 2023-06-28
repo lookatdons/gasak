a02f4ffbeb4edf288c26d86d3a34edbf2f516fee
lookatdons <lookatdons@users.noreply.github.com>
2023-06-28 15:35:28 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,2 +1,10 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2108912098413782"
-     crossorigin="anonymous"></script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '9a3f33d630109b817459793e891d4149',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sortingfantasticgreatly.com/9a3f33d630109b817459793e891d4149/invoke.js"></scr' + 'ipt>');
+</script>
