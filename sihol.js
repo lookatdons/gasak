f219f49e2b8a8b1b3d8c3d1260ece8d893bea3fb
lookatdons <lookatdons@users.noreply.github.com>
2023-09-05 11:22:49 +0700

    Update sihol.js

--- sihol.js
+++ sihol.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://amzn.to/3lIXfqf"); 
+      location.replace("https://circleoftoast.blogspot.com/"); 
     },10); 
   } 
 }, false); 
