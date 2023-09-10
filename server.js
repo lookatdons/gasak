e0d2a7ff75c4de1ba0ce5890784fa89990674ba3
lookatdons <lookatdons@users.noreply.github.com>
2023-09-10 15:52:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-<script type="text/javascript">
-	atOptions = {
+atOptions = {
 		'key' : '66d03a0154b09a9cdc94a1a16dbf5124',
 		'format' : 'iframe',
 		'height' : 250,
@@ -7,4 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//effacedefend.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
-</script>
