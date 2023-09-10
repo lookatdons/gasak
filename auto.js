c4a9bb2f3fb412e5308513bc64977525e156832e
lookatdons <lookatdons@users.noreply.github.com>
2023-09-10 08:48:12 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,10 +1,10 @@
-<script type="text/javascript">
+<center><script type="text/javascript">
 	atOptions = {
-		'key' : '9863b4c354792dae4b7dce4185ea87af',
+		'key' : '66d03a0154b09a9cdc94a1a16dbf5124',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//effacedefend.com/9863b4c354792dae4b7dce4185ea87af/invoke.js"></scr' + 'ipt>');
-</script>
+	document.write('<scr' + 'ipt type="text/javascript" src="//effacedefend.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
+</script></center>
