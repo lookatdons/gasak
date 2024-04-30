16a1f374ccd37b8d54e33018d7fa0b783bbdc20d
lookatdons <lookatdons@users.noreply.github.com>
2024-04-30 19:55:33 +0700

    Create gemar.js

---
+++ gemar.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '406e847081d98fc313efc046ed55e9c4',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/406e847081d98fc313efc046ed55e9c4/invoke.js"></scr' + 'ipt>');
