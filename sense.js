0465d7b698539ddce0a7f7fa1c8f124b7844578b
lookatdons <lookatdons@users.noreply.github.com>
2024-08-07 21:05:07 +0700

    Update sense.js

--- sense.js
+++ sense.js
@@ -1,1 +1,71 @@
-<center><ins data-ad-format="auto" class="adsbygoogle adsbygoogle-noablate" data-ad-client="ca-pub-2719456103820692" data-adsbygoogle-status="done" style="display: block; margin: auto; background-color: transparent; height: 280px;" data-ad-status="filled"><div id="aswift_1_host" style="border: none; height: 280px; width: 619px; margin: 0px; padding: 0px; position: relative; visibility: visible; background-color: transparent; display: inline-block; overflow: visible;"><iframe id="aswift_1" name="aswift_1" browsingtopics="true" style="left:0;position:absolute;top:0;border:0;width:619px;height:280px;" sandbox="allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation" width="619" height="280" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allow="attribution-reporting; run-ad-auction" src="https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-2719456103820692&amp;output=html&amp;h=280&amp;adk=3780643339&amp;adf=3721553632&amp;pi=t.aa~a.2824062698~i.41~rp.4&amp;w=619&amp;abgtt=6&amp;fwrn=4&amp;fwrnh=100&amp;lmt=1720759597&amp;num_ads=1&amp;rafmt=1&amp;armr=3&amp;sem=mc&amp;pwprc=2003229778&amp;ad_type=text_image&amp;format=619x280&amp;url=https%3A%2F%2Fugobepleo.co.uk%2F4059%2Fdo-it-smart-getting-the-best-car-insurance-wisely.html&amp;fwr=0&amp;pra=3&amp;rh=155&amp;rw=618&amp;rpe=1&amp;resp_fmts=3&amp;wgl=1&amp;fa=27&amp;uach=WyJXaW5kb3dzIiwiMTUuMC4wIiwieDg2IiwiIiwiMTI2LjAuNjQ3OC4xMjciLG51bGwsMCxudWxsLCI2NCIsW1siTm90L0EpQnJhbmQiLCI4LjAuMC4wIl0sWyJDaHJvbWl1bSIsIjEyNi4wLjY0NzguMTI3Il0sWyJHb29nbGUgQ2hyb21lIiwiMTI2LjAuNjQ3OC4xMjciXV0sMF0.&amp;dt=1720773292488&amp;bpp=2&amp;bdt=4200&amp;idt=-M&amp;shv=r20240709&amp;mjsv=m202407080101&amp;ptt=9&amp;saldr=aa&amp;abxe=1&amp;cookie_enabled=1&amp;eoidce=1&amp;prev_fmts=0x0&amp;nras=2&amp;correlator=3619079163649&amp;frm=20&amp;pv=1&amp;u_tz=480&amp;u_his=1&amp;u_h=720&amp;u_w=1280&amp;u_ah=672&amp;u_aw=1280&amp;u_cd=24&amp;u_sd=1.5&amp;dmc=4&amp;adx=155&amp;ady=2556&amp;biw=1263&amp;bih=551&amp;scr_x=0&amp;scr_y=1900&amp;eid=44759876%2C44759927%2C44759842%2C31084868%2C42531705%2C44795921%2C95334508%2C95334529%2C95334578%2C95334830%2C31085162%2C31078663%2C31078665%2C31078668%2C31078670&amp;oid=2&amp;pvsid=3851388399904540&amp;tmod=569946522&amp;uas=0&amp;nvt=1&amp;fc=1408&amp;brdim=0%2C0%2C0%2C0%2C1280%2C0%2C1280%2C672%2C1280%2C551&amp;vis=1&amp;rsz=%7C%7Cs%7C&amp;abl=NS&amp;fu=128&amp;bc=31&amp;bz=1&amp;td=1&amp;tdf=2&amp;psd=W251bGwsbnVsbCxudWxsLDNd&amp;nt=1&amp;ifi=2&amp;uci=a!2&amp;btvi=1&amp;fsb=1&amp;dtd=4" data-google-container-id="a!2" tabindex="0" title="Advertisement" aria-label="Advertisement" data-google-query-id="CKP4o4eMoYcDFUZLnQkdH0cAaQ" data-load-complete="true"></iframe></div></ins></center>
+(function() {
+    // Create the center element
+    var center = document.createElement('center');
+
+    // Create the ins element
+    var ins = document.createElement('ins');
+    ins.setAttribute('data-ad-format', 'auto');
+    ins.className = 'adsbygoogle adsbygoogle-noablate';
+    ins.setAttribute('data-ad-client', 'ca-pub-2719456103820692');
+    ins.setAttribute('data-adsbygoogle-status', 'done');
+    ins.style.display = 'block';
+    ins.style.margin = 'auto';
+    ins.style.backgroundColor = 'transparent';
+    ins.style.height = '280px';
+    ins.setAttribute('data-ad-status', 'filled');
+
+    // Create the div element
+    var div = document.createElement('div');
+    div.id = 'aswift_1_host';
+    div.style.border = 'none';
+    div.style.height = '280px';
+    div.style.width = '619px';
+    div.style.margin = '0px';
+    div.style.padding = '0px';
+    div.style.position = 'relative';
+    div.style.visibility = 'visible';
+    div.style.backgroundColor = 'transparent';
+    div.style.display = 'inline-block';
+    div.style.overflow = 'visible';
+
+    // Create the iframe element
+    var iframe = document.createElement('iframe');
+    iframe.id = 'aswift_1';
+    iframe.name = 'aswift_1';
+    iframe.style.left = '0';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.border = '0';
+    iframe.style.width = '619px';
+    iframe.style.height = '280px';
+    iframe.setAttribute('sandbox', 'allow-forms allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts allow-top-navigation-by-user-activation');
+    iframe.width = '619';
+    iframe.height = '280';
+    iframe.frameBorder = '0';
+    iframe.marginWidth = '0';
+    iframe.marginHeight = '0';
+    iframe.vSpace = '0';
+    iframe.hSpace = '0';
+    iframe.allowTransparency = 'true';
+    iframe.scrolling = 'no';
+    iframe.allow = 'attribution-reporting; run-ad-auction';
+    iframe.src = 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-2719456103820692&output=html&h=280&adk=3780643339&adf=3721553632&pi=t.aa~a.2824062698~i.41~rp.4&w=619&abgtt=6&fwrn=4&fwrnh=100&lmt=1720759597&num_ads=1&rafmt=1&armr=3&sem=mc&pwprc=2003229778&ad_type=text_image&format=619x280&url=https%3A%2F%2Fugobepleo.co.uk%2F4059%2Fdo-it-smart-getting-the-best-car-insurance-wisely.html&fwr=0&pra=3&rh=155&rw=618&rpe=1&resp_fmts=3&wgl=1&fa=27&uach=WyJXaW5kb3dzIiwiMTUuMC4wIiwieDg2IiwiIiwiMTI2LjAuNjQ3OC4xMjciLG51bGwsMCxudWxsLCI2NCIsW1siTm90L0EpQnJhbmQiLCI4LjAuMC4wIl0sWyJDaHJvbWl1bSIsIjEyNi4wLjY0NzguMTI3Il0sWyJHb29nbGUgQ2hyb21lIiwiMTI2LjAuNjQ3OC4xMjciXV0sMF0.&dt=1720773292488&bpp=2&bdt=4200&idt=-M&shv=r20240709&mjsv=m202407080101&ptt=9&saldr=aa&abxe=1&cookie_enabled=1&eoidce=1&prev_fmts=0x0&nras=2&correlator=3619079163649&frm=20&pv=1&u_tz=480&u_his=1&u_h=720&u_w=1280&u_ah=672&u_aw=1280&u_cd=24&u_sd=1.5&dmc=4&adx=155&ady=2556&biw=1263&bih=551&scr_x=0&scr_y=1900&eid=44759876%2C44759927%2C44759842%2C31084868%2C42531705%2C44795921%2C95334508%2C95334529%2C95334578%2C95334830%2C31085162%2C31078663%2C31078665%2C31078668%2C31078670&oid=2&pvsid=3851388399904540&tmod=569946522&uas=0&nvt=1&fc=1408&brdim=0%2C0%2C0%2C0%2C1280%2C0%2C1280%2C672%2C1280%2C551&vis=1&rsz=%7C%7Cs%7C&abl=NS&fu=128&bc=31&bz=1&td=1&tdf=2&psd=W251bGwsbnVsbCxudWxsLDNd&nt=1&ifi=2&uci=a!2&btvi=1&fsb=1&dtd=4';
+    iframe.setAttribute('data-google-container-id', 'a!2');
+    iframe.tabIndex = '0';
+    iframe.title = 'Advertisement';
+    iframe.setAttribute('aria-label', 'Advertisement');
+    iframe.setAttribute('data-google-query-id', 'CKP4o4eMoYcDFUZLnQkdH0cAaQ');
+    iframe.setAttribute('data-load-complete', 'true');
+
+    // Append the iframe to the div
+    div.appendChild(iframe);
+
+    // Append the div to the ins
+    ins.appendChild(div);
+
+    // Append the ins to the center
+    center.appendChild(ins);
+
+    // Append the center to the body
+    document.body.appendChild(center);
+})();
