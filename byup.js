c094bc72ae75b4b4e936bea55cca7dded09d1326
lookatdons <lookatdons@users.noreply.github.com>
2024-08-07 10:37:04 +0700

    Create byup.js

---
+++ byup.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9e84f4d9e9e06d959425e7151d32d077',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/9e84f4d9e9e06d959425e7151d32d077/invoke.js"></scr' + 'ipt>');
