1001cb7f08516b7c3b3e34db897b818889693cfe
lookatdons <lookatdons@users.noreply.github.com>
2024-08-08 09:00:29 +0700

    Update native.js

--- native.js
+++ native.js
@@ -1,15 +1,2 @@
-// Load the external script
-(function() {
-  var script = document.createElement('script');
-  script.async = true;
-  script.setAttribute('data-cfasync', 'false');
-  script.src = '//pl23963743.highratecpm.com/713e87e323721ed5f81ca8700083a4c8/invoke.js';
-  document.head.appendChild(script);
-})();
-
-// Add the container div
-(function() {
-  var container = document.createElement('div');
-  container.id = 'container-713e87e323721ed5f81ca8700083a4c8';
-  document.body.appendChild(container);
-})();
+document.write(`<script async="async" data-cfasync="false" src="//pl23963743.highratecpm.com/713e87e323721ed5f81ca8700083a4c8/invoke.js"></script>
+<div id="container-713e87e323721ed5f81ca8700083a4c8"></div>`);
