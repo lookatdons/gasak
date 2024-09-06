fc16ebe0279b67921d49b809e7f5ff3b0d8fb1f8
lookatdons <lookatdons@users.noreply.github.com>
2024-09-06 12:44:09 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://irrigatenotwithstandingcommit.com/pm1ymmgxk3?key=399b8c1c420dbe0751ab7f618e873eb2"); 
+      location.replace("https://s.click.aliexpress.com/e/_EwAAeZ3"); 
     },10); 
   } 
 }, false); 
