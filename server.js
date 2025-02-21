8f34b8943c3e13d197dde3a161d69a09932dd87b
lookatdons <lookatdons@users.noreply.github.com>
2025-02-21 16:39:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//irrigatenotwithstandingcommit.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//debatabletent.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
