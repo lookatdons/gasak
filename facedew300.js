c7dc18403d1644c391243734e49d6687af649123
lookatdons <lookatdons@users.noreply.github.com>
2025-02-21 16:40:22 +0700

    Update facedew300.js

--- facedew300.js
+++ facedew300.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//irrigatenotwithstandingcommit.com/62fd959ca286368ae7c419eef5d59291/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//debatabletent.com/62fd959ca286368ae7c419eef5d59291/invoke.js"></scr' + 'ipt>');
