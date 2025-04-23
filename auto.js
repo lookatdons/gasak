140b556cd0d9b278fbc0f26bcd212af63a297409
lookatdons <lookatdons@users.noreply.github.com>
2025-04-23 22:59:52 +0700

    Update auto.js

--- auto.js
+++ auto.js
@@ -6,5 +6,5 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//effacedefend.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//loopflavour.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
 </script></center>
