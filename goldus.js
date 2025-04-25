dc933dd455fcc2ce05fac66a6a8a18d1e194789c
lookatdons <lookatdons@users.noreply.github.com>
2025-04-25 14:37:13 +0700

    Create goldus.js

---
+++ goldus.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9fb20fb9fd4e25e7a0c3aa6cfab48834',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//loopflavour.com/9fb20fb9fd4e25e7a0c3aa6cfab48834/invoke.js"></scr' + 'ipt>');
