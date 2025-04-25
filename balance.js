edd9f9abffe8a67a163cb5f654f7ee75ff37f613
lookatdons <lookatdons@users.noreply.github.com>
2025-04-25 12:23:06 +0700

    Create balance.js

---
+++ balance.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '34bbf7b5245c51324fb683a01f54f687',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//postponeclement.com/34bbf7b5245c51324fb683a01f54f687/invoke.js"></scr' + 'ipt>');
