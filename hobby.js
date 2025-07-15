031e0d21febe37f96c26cd0484a2c052a689dc48
lookatdons <lookatdons@users.noreply.github.com>
2025-07-15 21:39:55 +0700

    Update hobby.js

--- hobby.js
+++ hobby.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//postponeclement.com/c160cb85beae5d49f08aeb93156fe646/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//sneakintriguecasting.com/c160cb85beae5d49f08aeb93156fe646/invoke.js"></scr' + 'ipt>');
