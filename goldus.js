a603d69fae3b65faec4baa617eb2cd6fa8bd1c7f
lookatdons <lookatdons@users.noreply.github.com>
2025-08-26 09:05:55 +0700

    Update goldus.js

--- goldus.js
+++ goldus.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 468,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//loopflavour.com/9fb20fb9fd4e25e7a0c3aa6cfab48834/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//suggestbingo.com/9fb20fb9fd4e25e7a0c3aa6cfab48834/invoke.js"></scr' + 'ipt>');
