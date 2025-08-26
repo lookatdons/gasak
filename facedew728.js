65982312277b33b2e0945d80043ef49e3de27a2e
lookatdons <lookatdons@users.noreply.github.com>
2025-08-26 09:07:19 +0700

    Update facedew728.js

--- facedew728.js
+++ facedew728.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 320,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//loopflavour.com/bc60cb154552a382a0225da0907dbea7/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//suggestbingo.com/bc60cb154552a382a0225da0907dbea7/invoke.js"></scr' + 'ipt>');
