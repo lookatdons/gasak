851d84914874f723c9149e36f5d2110b350072c1
lookatdons <lookatdons@users.noreply.github.com>
2025-08-26 08:30:21 +0700

    Update agcai.js

--- agcai.js
+++ agcai.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//sneakintriguecasting.com/b3d21d3db89b7ea791ec62f4f2098b6f/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//kickhoistsuccess.com/b3d21d3db89b7ea791ec62f4f2098b6f/invoke.js"></scr' + 'ipt>');
