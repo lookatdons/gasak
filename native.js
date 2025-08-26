ea4f6c4b9a620f8e148253ddcb2717134503401c
lookatdons <lookatdons@users.noreply.github.com>
2025-08-26 08:59:38 +0700

    Update native.js

--- native.js
+++ native.js
@@ -1,2 +1,2 @@
-document.write(`<script async="async" data-cfasync="false" src="//sneakintriguecasting.com/d60ff2c8749647e766ce091b6eacbaff/invoke.js"></script>
+document.write(`<script async="async" data-cfasync="false" src="//kickhoistsuccess.com/d60ff2c8749647e766ce091b6eacbaff/invoke.js"></script>
 <div id="container-d60ff2c8749647e766ce091b6eacbaff"></div>`);
