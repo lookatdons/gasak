d68f319403aa4a39ad0b445892bc473152856e6d
lookatdons <lookatdons@users.noreply.github.com>
2025-09-16 19:15:33 +0700

    Create dust.js

---
+++ dust.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '9863b4c354792dae4b7dce4185ea87af',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//suggestbingo.com/9863b4c354792dae4b7dce4185ea87af/invoke.js"></scr' + 'ipt>');
