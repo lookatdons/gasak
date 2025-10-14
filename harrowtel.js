56932dcc09db6a1ee7002b076b45c383e9d35ad5
lookatdons <lookatdons@users.noreply.github.com>
2025-10-14 14:18:16 +0700

    Create harrowtel.js

---
+++ harrowtel.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c0e232b9c17167c4ed6b406a69d29a5d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//spendharassingwarmly.com/c0e232b9c17167c4ed6b406a69d29a5d/invoke.js"></scr' + 'ipt>');
