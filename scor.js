87b7bc2ea04c9566f4a608db01cead2617381cd4
lookatdons <lookatdons@users.noreply.github.com>
2025-10-22 11:05:55 +0700

    Create scor.js

---
+++ scor.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '5b23d49dd36e199223f743e1d9b73f2d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//hardfinbanana.com/5b23d49dd36e199223f743e1d9b73f2d/invoke.js"></scr' + 'ipt>');
