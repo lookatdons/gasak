3843329ac4081253b957e8d091e653d4abbe2dc8
lookatdons <lookatdons@users.noreply.github.com>
2025-11-06 12:26:06 +0700

    Create free.js

---
+++ free.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1ed2e5bfb6c191f45e4e3c8ff67c233b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//migrationoctavian.com/1ed2e5bfb6c191f45e4e3c8ff67c233b/invoke.js"></scr' + 'ipt>');
