d3d02fe0e70521c2f8d5635b3ed4f0754e74ab85
lookatdons <lookatdons@users.noreply.github.com>
2025-12-25 20:54:34 +0700

    Update facedew300.js

--- facedew300.js
+++ facedew300.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '62fd959ca286368ae7c419eef5d59291',
+		'key' : 'e66c99cdf7808557dd39b265ad60d114',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//suggestbingo.com/62fd959ca286368ae7c419eef5d59291/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//foreseehawancestor.com/e66c99cdf7808557dd39b265ad60d114/invoke.js"></scr' + 'ipt>');
