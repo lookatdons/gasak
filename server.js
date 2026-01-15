33308ad894b5e58b9c34d6613f94ce3b5a7309bc
lookatdons <lookatdons@users.noreply.github.com>
2026-01-15 11:50:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'e66c99cdf7808557dd39b265ad60d114',
+		'key' : '66d03a0154b09a9cdc94a1a16dbf5124',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//foreseehawancestor.com/e66c99cdf7808557dd39b265ad60d114/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//forgottenoverlive.com/66d03a0154b09a9cdc94a1a16dbf5124/invoke.js"></scr' + 'ipt>');
